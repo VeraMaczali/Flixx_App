e6cffb0525f705982d4c2e733cb1f737ba769c26
Vera Maczali <maczalivera@gmail.com>
2024-01-05 15:36:24 +0100

    Add slider

--- js/script.js
+++ js/script.js
@@ -151,7 +151,7 @@ async function displayMovieDetails() {
   displayBackgroundImage("movie", movie.backdrop_path);
 
   const div = document.createElement("div");
-  div.innerHTML += `
+  div.innerHTML = `
   <div class="details-top">
     <div>
     ${
@@ -234,6 +234,57 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+//Display Slider Movies
+async function displaySlider() {
+  const { results } = await fetchAPIData("movie/now_playing");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `
+    <a href="movie-details.html?id=${movie.id}">
+    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${
+      movie.title
+    }" />
+  </a>
+  <h4 class="swiper-rating">
+    <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+      1
+    )} / 10
+  </h4>
+    `;
+    document.querySelector(".swiper-wrapper").appendChild(div);
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+      1400: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   /* Register your own key for free at https://themoviedb.org */
@@ -275,6 +326,7 @@ function init() {
     case "/":
     case "/index.html":
       displayPopularMovies();
+      displaySlider();
       break;
     case "/shows.html":
     case "/shows":
